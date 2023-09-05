6d55a7f9fc779426e21e4f8a00dc7438d20812df
Dell <Dell@DESKTOP-1F12SMQ>
2023-09-05 13:32:54 +0530

    testng

---
+++ Jav2/src/AA/Santhu.java
@@ -0,0 +1,16 @@
+package AA;
+
+public class Santhu {
+
+	public static void main(String[] args) {
+	
+		int length = 5;
+		int breadth =6;
+		int area =(length*breadth)/2;
+		
+		System.err.println("area of triangle is"+area+"sqms");
+	}		
+		
+		
+
+}
